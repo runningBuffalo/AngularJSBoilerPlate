499290bca4739251707f97a92c7fa2125d8381df
george <georgeheron@georges-MacBook-Pro.local>
2019-06-29 23:20:33 -0500

    Add connection string

--- Project/Models/IssueModel.cs
+++ Project/Models/IssueModel.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Dapper;
 
 namespace Project.Models
 {
     // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class IssueModel
     {
-        private readonly RequestDelegate _next;
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime? DateCreated { get; set; }
+
+        private static readonly string connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=Test123;Integrated Security=True";
+
+        public static IEnumerable<IssueModel> GetStuff()
+        {
+            var connection = new SqlConnection(connectionString);
+
+            string sql = @"select top(10) * from tblState;";
+            var issues = connection.Query<IssueModel>(sql);
+            return issues;
+
+        }
     }
 }
